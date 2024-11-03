e7e8d179eeb67cf980c3add0730a81bf308027e8
Jason Vandeyar <jasonvandeyar55@gmail.com>
2024-11-03 21:53:44 +0200

    Do-over

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Select from "react-select";
+import axios from "axios";
+
+const options = [
+  { value: "rock", label: "rock" },
+  { value: "metal", label: "metal" },
+  { value: "hardcore", label: "hardcore" },
+  { value: "alternativerock", label: "alternativerock" },
+  { value: "electronic", label: "electronic" },
+  { value: "punk", label: "punk" },
+  { value: "hardcorepunk", label: "hardcorepunk" },
+  { value: "folk", label: "folk" },
+  { value: "newage", label: "newage" },
+  { value: "classical", label: "classical" },
+  { value: "pop", label: "pop" },
+  { value: "choral", label: "choral" },
+  { value: "gospel", label: "gospel" },
+];
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [music, setMusic] = useState([]);
+  const [showPlayer, setShowPlayer] = useState(false);
+  const client_id = process.env.CLIENT_ID;
+  const songs = [];
+
+  const makeRequest = (e) => {
+    e.preventDefault();
+
+    axios
+      .get("https://api.jamendo.com/v3.0/tracks", {
+        params: {
+          client_id: client_id,
+          format: "json",
+          limit: 30,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <div className="row">
+        <div className="col-4">
+          <h2>Mood Music</h2>
+          <form onSubmit={makeRequest} className="row g-3">
+            <div className="col-auto">
+              <Select
+                defaultValue={selectedOption}
+                onChange={setSelectedOption}
+                options={options}
+                isMulti
+                placeholder="Choose your mood..."
+                required
+              />
+            </div>
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary">
+                Search
+              </button>
+            </div>
+          </form>
+          {showPlayer && (
+            <audio controls>
+              <source
+                src="https://prod-1.storage.jamendo.com/?trackid=168&format=mp31&from=aadQ%2FcXbwHety91GwXg%2BMw%3D%3D%7C4vC%2B9FTXDXVtQNWoqmbwog%3D%3D"
+                type="audio/mp3"
+              />
+            </audio>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
