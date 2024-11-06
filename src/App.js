ef3fe89764f55438fad20662b016609f3eac895b
Jason Vandeyar <jasonvandeyar55@gmail.com>
2024-11-06 04:23:57 +0200

    Adding updated player, still need to add songs and images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Select from "react-select";
-import axios from "axios";
+import Player from "./components/Player";
 
 const options = [
   { value: "rock", label: "rock" },
@@ -20,63 +19,16 @@ const options = [
 ];
 
 function App() {
-  const [selectedOption, setSelectedOption] = useState(null);
   const [music, setMusic] = useState([]);
   const [showPlayer, setShowPlayer] = useState(false);
+  // CREATE A .ENV FILE AND INCLUDE YOUR JAMENDO CLIENT ID
   const client_id = process.env.REACT_APP_CLIENT_ID;
   const songs = [];
 
-  const makeRequest = (e) => {
-    e.preventDefault();
-
-    axios
-      .get("https://api.jamendo.com/v3.0/tracks", {
-        params: {
-          client_id: client_id,
-          format: "json",
-          limit: 5,
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
   return (
     <div className="App container">
-      <div className="row">
-        <div className="col-4">
-          <h2>Mood Music</h2>
-          <form onSubmit={makeRequest} className="row g-3">
-            <div className="col-auto">
-              <Select
-                defaultValue={selectedOption}
-                onChange={setSelectedOption}
-                options={options}
-                isMulti
-                placeholder="Choose your mood..."
-                required
-              />
-            </div>
-            <div className="col-auto">
-              <button type="submit" className="btn btn-primary">
-                Search
-              </button>
-            </div>
-          </form>
-          {showPlayer && (
-            <audio controls>
-              <source
-                src="https://prod-1.storage.jamendo.com/?trackid=168&format=mp31&from=aadQ%2FcXbwHety91GwXg%2BMw%3D%3D%7C4vC%2B9FTXDXVtQNWoqmbwog%3D%3D"
-                type="audio/mp3"
-              />
-            </audio>
-          )}
-        </div>
-      </div>
+      <h2>Mood Music</h2>
+      <Player />
     </div>
   );
 }
